ddc71e1453e27935cf1814818b76e744f207415d
1nv8rZim <msf9542@rit.edu>
2023-10-18 14:52:54 -0400

    add span for birthday slash command

--- commands/slash/birthday.go
+++ commands/slash/birthday.go
@@ -3,6 +3,7 @@ package slash
 import (
 	"github.com/bwmarrin/discordgo"
 	"github.com/ritsec/ops-bot-iii/commands/slash/permission"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
 // Birthday is a slash command that allows users to add, edit, or remove their birthday
@@ -13,6 +14,11 @@ func Birthday() (*discordgo.ApplicationCommand, func(s *discordgo.Session, i *di
 			DefaultMemberPermissions: &permission.Member,
 		},
 		func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			span := tracer.StartSpan(
+				"commands.slash.birthday:Birthday",
+				tracer.ResourceName("/birthday"),
+			)
+			defer span.Finish()
 
 		}
 }
