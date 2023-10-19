ccc7c0ebf051bca932f49daeeebe514b19160bd9
Hal Williams <90720910+hfw8271@users.noreply.github.com>
2023-10-19 02:05:34 -0400

    Midnight loop

--- commands/scheduled/birthday.go
+++ commands/scheduled/birthday.go
@@ -3,6 +3,8 @@ package scheduled
 import (
 	"github.com/bwmarrin/discordgo"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+	//Importing time to figure out when its midnight
+	"time"
 )
 
 // Birthday is a scheduled event that runs at midnight to remove existing birthday roles and add new ones
@@ -13,7 +15,30 @@ func Birthday(s *discordgo.Session, quit chan interface{}) error {
 	)
 	defer span.Finish()
 
-	// code here
+	//Hal Williams
+	for {
+
+		// Get the time
+		currentTime := time.Now()
+
+		// Calculate midnight
+		midnight := time.Date(currentTime.Yea(), currentTime.Month(), currentTime.Day(), 0, 0, 0, 0, time.Local)
+		if midnight.Before(currentTime) {
+			midnight = midnight.Add(24 * time.Hour)
+		}
+
+		// Calculate the time to midnight
+		timeToMidnight := midnight.Sub(currentTime)
+
+		//	Ticker to add and remove birthday roles at midnight
+		ticker := time.NewTicker(timeToMidnight)
+
+		select {
+		case <-ticker.C:
+			//birthday add and remove here
+		}
+	}
+
 
 	return nil
 }
