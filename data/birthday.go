22aaad63d7932880d2d201ffac77d68ab637c0e8
1nv8rZim <msf9542@rit.edu>
2023-11-08 16:50:44 -0500

    birthday abstractions

--- data/birthday.go
+++ data/birthday.go
@@ -2,6 +2,8 @@ package data
 
 import (
 	"github.com/ritsec/ops-bot-iii/ent"
+	"github.com/ritsec/ops-bot-iii/ent/birthday"
+	"github.com/ritsec/ops-bot-iii/ent/user"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
@@ -18,13 +20,17 @@ func (*birthday_s) Exists(user_id string, ctx ddtrace.SpanContext) (bool, error)
 	)
 	defer span.Finish()
 
-	// code here
-
-	return false, nil
+	return Client.Birthday.Query().
+		Where(
+			birthday.HasUserWith(
+				user.ID(user_id),
+			),
+		).
+		Exist(Ctx)
 }
 
 // GetBirthdays gets all birthdays for a given day and month
-func (*birthday_s) GetBirthdays(day int, month int, ctx ddtrace.SpanContext) ([]ent.Birthday, error) {
+func (*birthday_s) GetBirthdays(day int, month int, ctx ddtrace.SpanContext) ([]*ent.Birthday, error) {
 	span := tracer.StartSpan(
 		"data.birthday:GetBirthdays",
 		tracer.ResourceName("Data.Birthday.GetBirthdays"),
@@ -32,13 +38,17 @@ func (*birthday_s) GetBirthdays(day int, month int, ctx ddtrace.SpanContext) ([]
 	)
 	defer span.Finish()
 
-	// code here
-
-	return nil, nil
+	return Client.Birthday.Query().
+		Where(
+			birthday.Day(day),
+			birthday.Month(month),
+		).
+		WithUser().
+		All(Ctx)
 }
 
 // Create creates a new birthday for a user
-func (*birthday_s) Create(user_id string, day int, month int, ctx ddtrace.SpanContext) (ent.Birthday, error) {
+func (*birthday_s) Create(user_id string, day int, month int, ctx ddtrace.SpanContext) (*ent.Birthday, error) {
 	span := tracer.StartSpan(
 		"data.birthday:Create",
 		tracer.ResourceName("Data.Birthday.Create"),
@@ -46,13 +56,22 @@ func (*birthday_s) Create(user_id string, day int, month int, ctx ddtrace.SpanCo
 	)
 	defer span.Finish()
 
-	// code here
+	entUser, err := User.Get(user_id, ctx)
+	if err != nil {
+		return nil, err
+	}
 
-	return ent.Birthday{}, nil
+	return Client.Birthday.Create().
+		SetDay(day).
+		SetMonth(month).
+		SetUser(
+			entUser,
+		).
+		Save(Ctx)
 }
 
 // Get gets a birthday for a user
-func (*birthday_s) Get(user_id string, ctx ddtrace.SpanContext) (ent.Birthday, error) {
+func (*birthday_s) Get(user_id string, ctx ddtrace.SpanContext) (*ent.Birthday, error) {
 	span := tracer.StartSpan(
 		"data.birthday:Get",
 		tracer.ResourceName("Data.Birthday.Get"),
@@ -60,9 +79,14 @@ func (*birthday_s) Get(user_id string, ctx ddtrace.SpanContext) (ent.Birthday, e
 	)
 	defer span.Finish()
 
-	// code here
-
-	return ent.Birthday{}, nil
+	return Client.Birthday.Query().
+		Where(
+			birthday.HasUserWith(
+				user.ID(user_id),
+			),
+		).
+		WithUser().
+		Only(Ctx)
 }
 
 // Delete deletes a birthday for a user
@@ -74,7 +98,11 @@ func (*birthday_s) Delete(user_id string, ctx ddtrace.SpanContext) (int, error) 
 	)
 	defer span.Finish()
 
-	// code here
-
-	return 0, nil
+	return Client.Birthday.Delete().
+		Where(
+			birthday.HasUserWith(
+				user.ID(user_id),
+			),
+		).
+		Exec(Ctx)
 }
