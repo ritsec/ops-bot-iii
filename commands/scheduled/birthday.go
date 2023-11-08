3929c03e1d956aa34ebea87b6106e8e9e2e07e81
Hal Williams <90720910+hfw8271@users.noreply.github.com>
2023-11-08 17:18:07 -0500

    bday add/remove

--- commands/scheduled/birthday.go
+++ commands/scheduled/birthday.go
@@ -7,6 +7,25 @@ import (
 	"time"
 )
 
+
+func removeBirthday(GuildID, UserID, birthdayRoleID string) error {
+	err := s.GuildMemberRoleRemove(GuildID, UserID, birthdayRoleID)
+	if err != nil {
+		logging.Error(s, err.Error(), nil, span)
+		return err
+	}
+	return nil
+}
+
+func addBirthday(GuildID, UserID, birthdayRoleID string) error {
+	err := s.GuildMemberRoleAdd(GuildID, UserID, birthdayRoleID)
+	if err != nil {
+		logging.Error(s, err.Error(), nil, span)
+		return err
+	}
+	return nil
+}
+
 // Birthday is a scheduled event that runs at midnight to remove existing birthday roles and add new ones
 func Birthday(s *discordgo.Session, quit chan interface{}) error {
 	span := tracer.StartSpan(
@@ -25,10 +44,33 @@ func Birthday(s *discordgo.Session, quit chan interface{}) error {
 	c := cron.NewWithLocation(est)
 
 	err = c.AddFunc("0 0 0 * * *", func() {
-		//call two functions, first one to remove all roles
-		//second one to add the new birthday roles
+		currentTime := time.Now()
+
+		month := currentTime.Month()
+		day := currentTime.Day()
+
+		
+		//loop through all birthday users from the prior day
+			//for each user run removeBirthday()
+			//remove all stored birthdays
+		//obtain all current birthdays
+		
+		entBirthday, err := data.getBirthday(month, day, ctx)
+		if err != nill {
+			logging.Error(s, err.Error(), nil, span)
+			return err
+		}
+
+		//store all birthdays
+
+		//loop through all new stored birthdays
+		for _, entBirthday := range entBirthday {
+			addBirthday(config.GuildID, entBirthday.Edges.User.ID, "birthdayID")
+		}
+			//for each user run addBirthday
 	})
-	if err != nil {}
+	if err != nil {
+	logging.Error(s, err.Error(), nil, span)
 	return err
 	}
 
