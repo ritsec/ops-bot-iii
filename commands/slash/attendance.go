1bb0654a8750231a70d4abf406c84a96a3c26ab4
FuRyNGs <silsbeeowen@gmail.com>
2024-09-29 17:15:16 -0400

    still fixing

--- commands/slash/attendance.go
+++ commands/slash/attendance.go
@@ -2,19 +2,12 @@ package slash
 
 import (
 	"fmt"
-	"strings"
-	"time"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/google/uuid"
 	"github.com/ritsec/ops-bot-iii/commands/slash/permission"
-	"github.com/ritsec/ops-bot-iii/config"
 	"github.com/ritsec/ops-bot-iii/data"
 	"github.com/ritsec/ops-bot-iii/ent/signin"
-	"github.com/ritsec/ops-bot-iii/google"
-	"github.com/ritsec/ops-bot-iii/helpers"
 	"github.com/ritsec/ops-bot-iii/logging"
-	"github.com/sirupsen/logrus"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
