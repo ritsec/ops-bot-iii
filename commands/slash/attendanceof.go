7998135576f89badd9d8e803a3ea1441dec4a569
FuRyNGs <silsbeeowen@gmail.com>
2024-10-20 09:45:07 -0400

    updatedmessage

--- commands/slash/attendanceof.go
+++ commands/slash/attendanceof.go
@@ -38,7 +38,7 @@ func Attendanceof() (*discordgo.ApplicationCommand, func(s *discordgo.Session, i
 			&discordgo.InteractionResponse {
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
 				Data: &discordgo.InteractionResponseData {
-					Content: attendanceMessage(u.ID, span.Context()),
+					Content: attendanceofMessage(u, span.Context()),
 					Flags: discordgo.MessageFlagsEphemeral,
 				},
 			},
@@ -49,4 +49,86 @@ func Attendanceof() (*discordgo.ApplicationCommand, func(s *discordgo.Session, i
 			logging.Debug(s, "Signin History Given for " + u.Username, i.Member.User, span)
 		}
 	}
+}
+
+//returns the message sent to the user by the Attendanceof command
+func attendanceofMessage(u User, ctx ddtrace.SpanContext) (message string) {
+	span := tracer.StartSpan(
+		"commands.slash.attendanceof:attendanceofMessage",
+		tracer.ResourceName("/attendanceof:attendanceofMessage"),
+		tracer.ChildOf(ctx),
+	)
+	defer span.Finish()
+
+	message = ("**" + u.Username + "Signins:**")
+	signinTypes := [...]string{
+		"General Meeting",
+		"Contagion",
+		"IR",
+		"Ops",
+		"Ops IG",
+		"Red Team",
+		"Red Team Recruiting",
+		"RVAPT",
+		"Reversing",
+		"Physical",
+		"Wireless",
+		"WiCyS",
+		"Vulnerability Research",
+		"Mentorship",
+		"Other",
+	}
+
+	totalSignins, err := data.Signin.GetSignins(u.ID, span.Context())
+	if err != nil {
+		logging.Error(nil, err.Error(), nil, span)
+		totalSignins = 0
+	}
+	message += fmt.Sprintf("\n\tTotal Signins: `%d`", totalSignins)
+
+	for _, signinType := range signinTypes {
+		var entSigninType signin.Type
+			switch signinType {
+			case "General Meeting":
+				entSigninType = signin.TypeGeneralMeeting
+			case "Contagion":
+				entSigninType = signin.TypeContagion
+			case "IR":
+				entSigninType = signin.TypeIR
+			case "Ops":
+				entSigninType = signin.TypeOps
+			case "Ops IG":
+				entSigninType = signin.TypeOpsIG
+			case "Red Team":
+				entSigninType = signin.TypeRedTeam
+			case "Red Team Recruiting":
+				entSigninType = signin.TypeRedTeamRecruiting
+			case "RVAPT":
+				entSigninType = signin.TypeRVAPT
+			case "Reversing":
+				entSigninType = signin.TypeReversing
+			case "Physical":
+				entSigninType = signin.TypePhysical
+			case "Wireless":
+				entSigninType = signin.TypeWireless
+			case "WiCyS":
+				entSigninType = signin.TypeWiCyS
+			case "Vulnerability Research":
+				entSigninType = signin.TypeVulnerabilityResearch
+			case "Mentorship":
+				entSigninType = signin.TypeMentorship
+			case "Other":
+				entSigninType = signin.TypeOther
+			}
+		signins, err := data.Signin.GetSigninsByType(u.ID, entSigninType, span.Context())
+		if err != nil {
+			logging.Error(nil, err.Error(), nil, span)
+			signins = 0
+		}
+		if signins != 0 {
+			message += fmt.Sprintf("\n\t%s: `%d`", signinType, signins)
+		}
+	}
+
+	return message
 }
