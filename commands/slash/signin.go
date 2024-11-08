f267c70c74d3a86061eb2adac25b14000b91e09e
FuRyNGs <silsbeeowen@gmail.com>
2024-11-07 22:01:27 -0500

    helper for signin types

--- commands/slash/signin.go
+++ commands/slash/signin.go
@@ -114,39 +114,7 @@ func Signin() (*discordgo.ApplicationCommand, func(s *discordgo.Session, i *disc
 
 			signinSlug := uuid.New().String()
 
-			var entSigninType signin.Type
-			switch signinType {
-			case "General Meeting":
-				entSigninType = signin.TypeGeneralMeeting
-			case "Contagion":
-				entSigninType = signin.TypeContagion
-			case "IR":
-				entSigninType = signin.TypeIR
-			case "Ops":
-				entSigninType = signin.TypeOps
-			case "Ops IG":
-				entSigninType = signin.TypeOpsIG
-			case "Red Team":
-				entSigninType = signin.TypeRedTeam
-			case "Red Team Recruiting":
-				entSigninType = signin.TypeRedTeamRecruiting
-			case "RVAPT":
-				entSigninType = signin.TypeRVAPT
-			case "Reversing":
-				entSigninType = signin.TypeReversing
-			case "Physical":
-				entSigninType = signin.TypePhysical
-			case "Wireless":
-				entSigninType = signin.TypeWireless
-			case "WiCyS":
-				entSigninType = signin.TypeWiCyS
-			case "Vulnerability Research":
-				entSigninType = signin.TypeVulnerabilityResearch
-			case "Mentorship":
-				entSigninType = signin.TypeMentorship
-			case "Other":
-				entSigninType = signin.TypeOther
-			}
+			entSigninType := helpers.StringToType(signinType)
 
 			// Check if sign-in creator has already signed in
 			recentSignin, err := data.Signin.RecentSignin(i.Member.User.ID, entSigninType, span.Context())
---
+++ helpers/signintype.go
@@ -0,0 +1,62 @@
+package helpers
+
+import "github.com/ritsec/ops-bot-iii/ent/signin"
+
+func StringToType(signinString string) signin.Type {
+	var entSigninType signin.Type
+	switch signinString {
+	case "General Meeting":
+		entSigninType = signin.TypeGeneralMeeting
+	case "Contagion":
+		entSigninType = signin.TypeContagion
+	case "IR":
+		entSigninType = signin.TypeIR
+	case "Ops":
+		entSigninType = signin.TypeOps
+	case "Ops IG":
+		entSigninType = signin.TypeOpsIG
+	case "Red Team":
+		entSigninType = signin.TypeRedTeam
+	case "Red Team Recruiting":
+		entSigninType = signin.TypeRedTeamRecruiting
+	case "RVAPT":
+		entSigninType = signin.TypeRVAPT
+	case "Reversing":
+		entSigninType = signin.TypeReversing
+	case "Physical":
+		entSigninType = signin.TypePhysical
+	case "Wireless":
+		entSigninType = signin.TypeWireless
+	case "WiCyS":
+		entSigninType = signin.TypeWiCyS
+	case "Vulnerability Research":
+		entSigninType = signin.TypeVulnerabilityResearch
+	case "Mentorship":
+		entSigninType = signin.TypeMentorship
+	case "Other":
+		entSigninType = signin.TypeOther
+	}
+	return entSigninType
+}
+
+
+func SigninTypeArray() [15]string {
+	return [...]string{
+		"General Meeting",
+		"Contagion",
+		"IR",
+		"Ops",
+		"Ops IG",
+		"Red Team",
+		"Red Team Recruiting",
+		"RVAPT",
+		"Reversing",
+		"Physical",
+		"Wireless",
+		"WiCyS",
+		"Vulnerability Research",
+		"Mentorship",
+		"Other",
+	}
+	
+}
