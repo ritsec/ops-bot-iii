5c4f8ef7739b3ba7f5e465c540ce7b7e3fb11821
Lukas Peters <peterslukas0512@gmail.com>
2024-11-08 16:21:48 -0500

    deleted sourceRc and just did setenvs for openstack variables

--- helpers/openstack.go
+++ helpers/openstack.go
@@ -3,16 +3,37 @@ package helpers
 import (
 	"bytes"
 	"io"
+	"os"
 	"os/exec"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/ritsec/ops-bot-iii/config"
 	"github.com/ritsec/ops-bot-iii/logging"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace"
 )
 
+var (
+	// Environment variables required for openstack CLI
+	OS_AUTH_URL             string = config.GetString("openstack.ENV.OS_AUTH_URL")
+	OS_PROJECT_ID           string = config.GetString("openstack.ENV.OS_PROJECT_ID")
+	OS_PROJECT_NAME         string = config.GetString("openstack.ENV.OS_PROJECT_NAME")
+	OS_USER_DOMAIN_NAME     string = config.GetString("openstack.ENV.OS_USER_DOMAIN_NAME")
+	OS_PROJECT_DOMAIN_ID    string = config.GetString("openstack.ENV.OS_PROJECT_DOMAIN_ID")
+	OS_USERNAME             string = config.GetString("openstack.ENV.OS_USERNAME")
+	OS_PASSWORD             string = config.GetString("openstack.ENV.OS_PASSWORD")
+	OS_REGION_NAME          string = config.GetString("openstack.ENV.OS_REGION_NAME")
+	OS_INTERFACE            string = config.GetString("openstack.ENV.OS_INTERFACE")
+	OS_IDENTITY_API_VERSION string = config.GetString("openstack.ENV.OS_IDENTITY_API_VERSION")
+
+	// Paths for scripts to automate openstack user management
+	new_member      string = config.GetString("openstack.SCRIPTS.new_member")
+	reset_password  string = config.GetString("openstack.SCRIPTS.reset_password")
+	check_if_exists string = config.GetString("openstack.SCRIPTS.check_if_exists")
+)
+
 func DebugCreate(s *discordgo.Session, user *discordgo.User, span ddtrace.Span, email string) (username string, password string, error error) {
-	createCmd := exec.Command("/root/automated_new_member.sh", email)
+	createCmd := exec.Command(new_member, email)
 
 	stdout := &bytes.Buffer{}
 	stderr := &bytes.Buffer{}
@@ -41,7 +62,7 @@ func DebugCreate(s *discordgo.Session, user *discordgo.User, span ddtrace.Span, 
 }
 
 func Create(email string) (username string, password string, error error) {
-	createCmd := exec.Command("/root/automated_new_member.sh", email)
+	createCmd := exec.Command(new_member, email)
 
 	stdout := &bytes.Buffer{}
 	stderr := &bytes.Buffer{}
@@ -65,7 +86,7 @@ func Create(email string) (username string, password string, error error) {
 }
 
 func Reset(email string) (username string, password string, error error) {
-	resetCmd := exec.Command("/root/automated_reset_password.sh", email)
+	resetCmd := exec.Command(reset_password, email)
 
 	stdout := &bytes.Buffer{}
 	stderr := &bytes.Buffer{}
@@ -85,7 +106,9 @@ func Reset(email string) (username string, password string, error error) {
 }
 
 func DebugCheckIfExists(s *discordgo.Session, user *discordgo.User, span ddtrace.Span, email string) (result bool, error error) {
-	checkIfExistsCmd := exec.Command("/root/automated_check_if_exists.sh", email)
+	checkIfExistsCmd := exec.Command(reset_password, email)
+
+	logging.Debug(s, checkIfExistsCmd.String(), user, span)
 
 	stdout := &bytes.Buffer{}
 	stderr := &bytes.Buffer{}
@@ -113,7 +136,7 @@ func DebugCheckIfExists(s *discordgo.Session, user *discordgo.User, span ddtrace
 }
 
 func CheckIfExists(email string) (result bool, error error) {
-	resetCmd := exec.Command("/root/automated_check_if_exists.sh", email)
+	resetCmd := exec.Command(check_if_exists, email)
 
 	stdout := &bytes.Buffer{}
 	stderr := &bytes.Buffer{}
@@ -133,37 +156,15 @@ func CheckIfExists(email string) (result bool, error error) {
 	}
 }
 
-func DebugSourceOpenRC(s *discordgo.Session, user *discordgo.User, span ddtrace.Span) error {
-	sourceCmd := exec.Command("bash", "-c", "source /root/ops-openrc.sh")
-
-	stdout := &bytes.Buffer{}
-	stderr := &bytes.Buffer{}
-	sourceCmd.Stdout = stdout
-	sourceCmd.Stderr = stderr
-	// Create a combined output buffer
-	combinedOutput := &bytes.Buffer{}
-	sourceCmd.Stdout = io.MultiWriter(stdout, combinedOutput)
-	sourceCmd.Stderr = io.MultiWriter(stderr, combinedOutput)
-
-	err := sourceCmd.Run()
-	if err != nil {
-		logging.Debug(s, combinedOutput.String(), user, span)
-		return err
-	}
-
-	logging.Debug(s, combinedOutput.String(), user, span)
-	return nil
-}
-
-func SourceOpenRC() error {
-	sourceCmd := exec.Command("bash", "-c", "source /root/ops-openrc.sh")
-
-	stderr := &bytes.Buffer{}
-	sourceCmd.Stderr = stderr
-
-	err := sourceCmd.Run()
-	if err != nil {
-		return err
-	}
-	return nil
+func SetOpenstackRC() {
+	os.Setenv("OS_AUTH_URL", OS_AUTH_URL)
+	os.Setenv("OS_PROJECT_ID", OS_PROJECT_ID)
+	os.Setenv("OS_PROJECT_NAME", OS_PROJECT_NAME)
+	os.Setenv("OS_USER_DOMAIN_NAME", OS_USER_DOMAIN_NAME)
+	os.Setenv("OS_PROJECT_DOMAIN_ID", OS_PROJECT_DOMAIN_ID)
+	os.Setenv("OS_USERNAME", OS_USERNAME)
+	os.Setenv("OS_PASSWORD", OS_PASSWORD)
+	os.Setenv("OS_REGION_NAME", OS_REGION_NAME)
+	os.Setenv("OS_INTERFACE", OS_INTERFACE)
+	os.Setenv("OS_IDENTITY_API_VERSION", OS_IDENTITY_API_VERSION)
 }
